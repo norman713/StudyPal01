9d642d174b86f0912abc8fe755da9df9e397ef5d
TranBaoTran <117694181+TranBaoTran@users.noreply.github.com>
2025-12-11 16:49:58 +0700

    feat: add configuration

---
+++ app.config.js
@@ -0,0 +1,59 @@
+export default ({ config }) => ({
+  ...config,
+  expo: {
+    name: "StudyPal",
+    slug: "StudyPal",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "studypal",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? "./android/app/google-services.json",
+      useNextNotificationsApi: true,
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      package: "com.greenpeas.StudyPal"
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      ["@react-native-google-signin/google-signin"],
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+          dark: {
+            backgroundColor: "#000000"
+          }
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "415c76c4-a125-4788-bc52-38c88821ab19"
+      }
+    }
+  }
+});
